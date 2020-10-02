f654a2db76f02ba66e184ff66099df90ea86fafa
Assaf Cohen <assafco@amdocs.com>
2020-10-02 13:02:04 +0300

    add submit button for new block

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,16 @@
-$('#newblock').click(function(){
-    $('#newblockblock').fadeIn(400);
-    $('#newblock').fadeOut(400);
+$('#new-block-btn').click(function(){
+    $('#new-block-wrapper').fadeIn(100);
+    // $('#new-block-btn').fadeOut(400);
+    // $.get( "new-block", function( data ) {
+    //     location.reload();
+    // });
+});
+
+$('#submit-btn').click(function(){
+    // $('#new-block-btn').fadeOut(400);
     $.get( "new-block", function( data ) {
         location.reload();
+        // $('#new-block-wrapper').fadeOut(100);
     });
 });
+
