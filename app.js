e146f8c538c5b546149c08486731efe220536f0f
Assaf Cohen <assafco@amdocs.com>
2020-10-05 18:59:00 +0300

    scss

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const sassMiddleware = require('node-sass-middleware');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -9,6 +10,15 @@ const config = require('./config');
 const fs = require('fs');
 
 const app = express();
+
+app.use(sassMiddleware({
+    /* Options */
+    src: 'scss',
+    dest: 'public',
+    debug: true,
+    outputStyle: 'extended',
+    // prefix:  '/prefix'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,10 +31,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 if (!fs.existsSync(config.data)) {
-  fs.writeFileSync(config.data, JSON.stringify({"blocks":[]}));
+    fs.writeFileSync(config.data, JSON.stringify({ "blocks": [] }));
 }
 if (!fs.existsSync(config.settings)) {
-  fs.writeFileSync(config.settings, JSON.stringify({"bg":"/images/bg8.jpg"}));
+    fs.writeFileSync(config.settings, JSON.stringify({ "bg": "/images/bg8.jpg" }));
 }
 
 fs.chmodSync(config.data, '777');
@@ -34,19 +44,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,57 +4,57 @@ const fs = require('fs');
 const config = require('../config');
 
 router.get('/', (req, res) => {
-  const data = JSON.parse(fs.readFileSync(config.data));
-  const settings = JSON.parse(fs.readFileSync(config.settings));
+    const data = JSON.parse(fs.readFileSync(config.data));
+    const settings = JSON.parse(fs.readFileSync(config.settings));
 
-  const bg = settings.bg ? settings.bg : '/images/bg1.jpg';
-  const bgUrl = `url('${bg}')`;
+    const bg = settings.bg ? settings.bg : '/images/bg1.jpg';
+    const bgUrl = `url('${bg}')`;
 
-  const blocks = JSON.parse(JSON.stringify(data.blocks));
+    const blocks = JSON.parse(JSON.stringify(data.blocks));
 
-  fs.readdirSync(config.userImages).forEach(file => {
-    console.log(file);
+    fs.readdirSync(config.userImages).forEach(file => {
+        console.log(file);
 
-    fs.copyFile(`${config.userImages}/${file}`, `./public/images/${file}`, (err) => {
-      if (err) throw err;
-      console.log('source.txt was copied to destination.txt');
+        fs.copyFile(`${config.userImages}/${file}`, `./public/images/${file}`, (err) => {
+            if (err) throw err;
+            console.log('source.txt was copied to destination.txt');
+        });
     });
-  });
 
-  blocks.forEach(block => {
-    block.link = block.link ? `openInNewTab(\'${block.link}\')` : '';
-    if (block.transperent && block.transperent === 'true') {
-      block.border = 'none';
-      block.boxshadow = 'none';
-      block.background = 'none';
-    }
-  });
+    blocks.forEach(block => {
+        block.link = block.link ? `openInNewTab(\'${block.link}\')` : '';
+        if (block.transperent && block.transperent === 'true') {
+            block.border = 'none';
+            block.boxshadow = 'none';
+            block.background = 'none';
+        }
+    });
 
-  res.render('index', { blocks, bgUrl, ...settings });
+    res.render('index', { blocks, bgUrl, ...settings });
 });
 
 router.post('/new-block', (req, res) => {
-  console.log(req.body);
+    console.log(req.body);
 
-  const { value, link, transperent, icon } = req.body;
+    const { value, link, transperent, icon } = req.body;
 
-  const data = JSON.parse(fs.readFileSync(config.data));
-  const blocks = JSON.parse(JSON.stringify(data.blocks));
-  blocks.push({
-    "name": value,
-    // "width": 100,
-    // "height": 100,
-    "text": "",
-    "link": link,
-    "icon": `/images/${icon}.png`,
-    "transperent": transperent
-  });
+    const data = JSON.parse(fs.readFileSync(config.data));
+    const blocks = JSON.parse(JSON.stringify(data.blocks));
+    blocks.push({
+        "name": value,
+        // "width": 100,
+        // "height": 100,
+        "text": "",
+        "link": link,
+        "icon": `/images/${icon}.png`,
+        "transperent": transperent
+    });
 
-  data.blocks = blocks;
+    data.blocks = blocks;
 
-  fs.writeFileSync(config.data, JSON.stringify(data));
+    fs.writeFileSync(config.data, JSON.stringify(data));
 
-  res.status(200).send();
+    res.status(200).send();
 });
 
 module.exports = router;
