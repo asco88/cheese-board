63bcf509dfb82e26bc606b1ddbff44468c3c9309
Assaf Cohen <assafco@amdocs.com>
2020-10-05 11:54:58 +0300

    chmod 777

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const config = require('./config');
+const fs = require('fs');
 
-var fs = require('fs');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,11 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, './cheese-board')));
-// app.use(express.static('C:\\msf\\cheese-dash\\cheese-board\\images'));
 
-if (!fs.existsSync(config.data))
+if (!fs.existsSync(config.data)) {
   fs.writeFileSync(config.data, JSON.stringify({"blocks":[]}));
+}
+fs.chmodSync(config.data, '777');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -36,7 +34,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
